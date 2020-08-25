682a4cba0b6db97c458bba1e9bea73978a4353b0
setocourtney <courtneyjseto@gmail.com>
2020-08-25 10:34:56 -0700

    create-react-app boilerplate

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from ('./pages/Home');
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route exact path = '/' component={Home} />
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
