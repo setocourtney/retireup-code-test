582a330068e1de507e6cf38c686f19cf82b1d874
setocourtney <courtneyjseto@gmail.com>
2020-08-25 10:34:21 -0700

    add script for seeding sp500 return data

---
+++ scripts/seed.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const db = require('../models/');
+const sp500Data = require('./sp500_returnData.js');
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/shareish", { useNewUrlParser: true, useUnifiedTopology: true });
+
+const seeds = sp500Data;
+
+db.Returns
+  .remove({})
+  .then(() => db.Returns.collection.insertMany(seeds))
+  .then(data => {
+    console.log(data.result.n + " items have been inserted!");
+    process.exit(0);
+  })
+  .catch(err => {
+    console.error(err);
+    process.exit(1);
+  });
